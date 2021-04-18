b0adcf97ca985eeff4636cf2c9c3d359cc01a551
Denys Grybov <denys@grybov.com>
2021-04-17 22:40:02 -0500

    Add i18n

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import React from 'react';
+import { appWithTranslation } from 'next-i18next';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default appWithTranslation(MyApp);
