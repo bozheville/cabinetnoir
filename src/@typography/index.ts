07286099ec8bc8c5c28ca6f843dcd2f6c6e21020
Denys Grybov <denys@grybov.com>
2021-06-12 02:37:14 +0200

    Add Minion typography component

--- src/@typography/index.ts
+++ src/@typography/index.ts
@@ -2,4 +2,5 @@ export { default as BodyCopy } from './BodyCopy';
 export { default as Canon } from './Canon';
 export { default as DoublePica } from './DoublePica';
 export { default as GreatPrimer } from './GreatPrimer';
+export { default as Minion } from './Minion';
 export { default as Trafalgar } from './Trafalgar';
