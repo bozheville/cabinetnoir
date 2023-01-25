2b1d0ee6545654e54b94b965fdcc423b7937ea97
Denys Grybov <denys.grybov@hellofresh.com>
2023-01-25 11:26:12 +0100

    update sitemap

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -34,7 +34,7 @@ const linksToXML = (links: string[]) => {
   let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
   for (const link of links) {
-    xml = xml + `\n  <sitemap>\n    <loc>${link}</loc>\n    <lastmod>${(new Date()).toISOString()}</lastmod>\n  </sitemap>`
+    xml = xml + `\n  <url>\n    <loc>${link}</loc>\n    <lastmod>${(new Date(parseInt(process.env.NEXT_PUBLIC_RELEASE_DATE, 10))).toISOString()}</lastmod>\n  </url>`
   }
 
   xml = xml + "\n</urlset>\n";
